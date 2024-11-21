8637c630a25cd4ee5d1c0a49d3e7c337a8b81dcb
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-11-21 22:41:56 +0000

    Add: React router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import AllJobsPage from './pages/AllJobsPage.jsx';
+import WelcomePage from './pages/WelcomePage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/all-jobs",
+		element: <AllJobsPage />,
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
---
+++ src/pages/AllJobsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function AllJobsPage() {
+  return (
+			<>
+				<div>
+					<Link to="/">Go to Welcome page</Link>
+				</div>
+				<div>This is All Jobs Page</div>
+			</>
+		);
+}
