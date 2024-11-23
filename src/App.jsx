ab2665460c745a0cf377666be72dc091c8a1d7ea
Oliver Craigie <ogc@live.ie>
2024-11-23 15:08:43 +0000

    Create components page and modal component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Trackerpage from "./components/Trackerpage";
 import Jobpage from "./components/Jobpage";
+import ComponentsPage from "./pages/ComponentsPage";
 import Preferencespage from "./components/Preferencespage";
 import { Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/tracker" element={<Trackerpage />} />
         <Route path="/jobs" element={<Jobpage />} />
         <Route path="/preferences" element={<Preferencespage />} />
+        <Route path="/components" element={<ComponentsPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/ComponentsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ConfirmModal from "../components/Modal";
+
+export default function ComponentsPage() {
+  return (
+    <>
+      <div>This is Components Page</div>
+      <ConfirmModal />
+    </>
+  );
+}
