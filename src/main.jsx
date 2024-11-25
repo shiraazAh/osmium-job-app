00950c537c35ae745e0c721b923c88b640accd44
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-11-25 22:47:28 +0000

    Add: Basic structure and ant design library

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { App as AntdApp } from "antd";
+import { ConfigProvider } from "antd";
+import { theme } from "./utils/theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConfigProvider theme={theme}>
+      <AntdApp style={{ height: "100vh" }}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AntdApp>
+    </ConfigProvider>
   </StrictMode>
 );
--- src/pages/AllJobsPage.jsx
+++ src/pages/AllJobsPage.jsx
@@ -1,13 +1,19 @@
+import { Card } from 'antd';
 import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function AllJobsPage() {
   return (
-			<>
-				<div>
-					<Link to="/">Go to Welcome page</Link>
-				</div>
-				<div>This is All Jobs Page</div>
-			</>
-		);
+    <>
+      <h3 className="text-white mt-3">Search & Land on your dream job</h3>
+      <div className="d-flex justify-content-center mt-4">
+        <Card className="shadow-sm" style={{ width: "100%", height: 200 }}>
+          <p>Input 1</p>
+          <p>Input 2</p>
+          <p>Input 3</p>
+          <p>Input 4</p>
+        </Card>
+      </div>
+    </>
+  );
 }
