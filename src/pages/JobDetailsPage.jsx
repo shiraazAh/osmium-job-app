2717bb52d33b69b9327e0ef091765c05de29782c
Oliver Craigie <ogc@live.ie>
2024-11-30 14:12:12 +0000

    Add details strcuture and add placeholder text

--- src/pages/JobDetailsPage.jsx
+++ src/pages/JobDetailsPage.jsx
@@ -14,8 +14,58 @@ export default function JobDetailsPage() {
         }}
       />
       <div>
-        {selected === "Description" && <h4>Description</h4>}
-        {selected === "Company" && <h4>Company</h4>}
+        {selected === "Description" && (
+          <div>
+            <div>
+              <h4>Responsibilities:</h4>
+              <ul>
+                <li>
+                  <p>
+                    You will design, develop, and test functions delivered by
+                    applications
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    You will work with our operations teams to ensure your
+                    services are available and reliable
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    You will develop hands-on experience in software development
+                    to perform at scale
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>Qualifications:</h4>
+              <ul>
+                <li>
+                  <p>Hands-on software development experience in Python</p>
+                </li>
+                <li>
+                  <p>Experience implementing unit and integration tests</p>
+                </li>
+                <li>
+                  <p>Experience with networking and security concepts</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+        {selected === "Company" && (
+          <div>
+            <h4>About Company</h4>
+            <p>
+              We take great pride in our culture here at Autodesk – our Culture
+              Code is at the core of everything we do. Our values and ways of
+              working help our people thrive and realize their potential, which
+              leads to even better outcomes for our customers.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
