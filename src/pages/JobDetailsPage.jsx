0879bb6ce35ea387784c0c6ab300d0de18e5fa89
Oliver Craigie <ogc@live.ie>
2024-11-30 14:01:58 +0000

    use Antd onChange to change the state when the other option is pressed

--- src/pages/JobDetailsPage.jsx
+++ src/pages/JobDetailsPage.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Segmented } from "antd";
 import "../styles.css";
 
 export default function JobDetailsPage() {
+  const [selected, setSelected] = useState("Description");
+
   return (
     <div>
       <Segmented
         options={["Description", "Company"]}
-        onChange={(value) => {
-          console.log(value);
+        onChange={(option) => {
+          setSelected(option);
         }}
       />
+      <div>
+        {selected === "Description" && <h4>Description</h4>}
+        {selected === "Company" && <h4>Company</h4>}
+      </div>
     </div>
   );
 }
