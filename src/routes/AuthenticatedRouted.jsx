58b985f8e7b59279c66eab18addbbf6250c69d18
Oliver Craigie <ogc@live.ie>
2024-12-02 11:25:41 +0000

    Add JobProvider to auth page

--- src/routes/AuthenticatedRouted.jsx
+++ src/routes/AuthenticatedRouted.jsx
@@ -3,38 +3,42 @@ import { Layout } from "antd";
 import CustomNavbar from "../components/Navbar";
 import AllJobsPage from "../pages/AllJobsPage";
 import ComponentsPage from "../pages/ComponentsPage";
+import { JobProvider } from "../context/JobContext";
+import JobPagination from "../components/JobPagination";
 import JobDetailsPage from "../pages/JobDetailsPage";
 
 export default function AuthenticatedRoutes() {
   return (
-    <>
-      <div
-        className="position-relative"
-        style={{ maxWidth: "375px", margin: "0 auto" }}
-      >
-        <CustomNavbar />
-      </div>
-      <Layout
-        style={{
-          minHeight: "100vh",
-          maxWidth: "375px",
-          margin: "0 auto",
-          padding: "0 20px",
-        }}
-      >
-        <Routes>
-          <Route path="/jobs" element={<AllJobsPage />} />
-          <Route path="/components" element={<ComponentsPage />} />
-          <Route path="/details" element={<JobDetailsPage />} />
-        </Routes>
-      </Layout>
-      {/*** Temporary ****/}
-      <div
-        className="position-fixed bottom-0 text-center w-100 shadow-lg d-flex align-items-center justify-content-center"
-        style={{ height: "70px" }}
-      >
-        This is the bottom bar
-      </div>
-    </>
+    <JobProvider>
+      <>
+        <div
+          className="position-relative"
+          style={{ maxWidth: "375px", margin: "0 auto" }}
+        >
+          <CustomNavbar />
+        </div>
+        <Layout
+          style={{
+            minHeight: "100vh",
+            maxWidth: "375px",
+            margin: "0 auto",
+            padding: "0 20px",
+          }}
+        >
+          <Routes>
+            <Route path="/components" element={<ComponentsPage />} />
+            <Route path="/jobs" element={<JobPagination />} />
+            <Route path="/job/:jobId" element={<JobDetailsPage />} />
+          </Routes>
+        </Layout>
+        {/*** Temporary ****/}
+        <div
+          className="position-fixed bottom-0 text-center w-100 shadow-lg d-flex align-items-center justify-content-center"
+          style={{ height: "70px" }}
+        >
+          This is the bottom bar
+        </div>
+      </>
+    </JobProvider>
   );
 }
