1e35cffa924aa3c0689f2ce78e0ffcd33f2aae0d
Oliver Craigie <ogc@live.ie>
2024-12-02 10:47:30 +0000

    Create useJobContext function, throws error if undefeined

--- src/context/JobContext.jsx
+++ src/context/JobContext.jsx
@@ -10,3 +10,11 @@ export const JobProvider = () => {
     </JobContext.Provider>
   );
 };
+
+export const useJobContext = () => {
+  const context = useContext(JobContext);
+  if (context === undefined) {
+    throw new Error("useJobContext must be used within a JobProvider");
+  }
+  return context;
+};
