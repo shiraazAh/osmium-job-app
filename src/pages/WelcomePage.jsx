8b9fbf12abe951704d5439bd7d3d414d5b2e9fce
SomeshGanti01 <HEMA.GANTI.2025@mumail.ie>
2024-12-03 15:40:08 +0000

    Welcome page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -3,15 +3,46 @@ import React from "react";
 
 export default function WelcomePage() {
   return (
+    <>
     <Layout
       style={{
         minHeight: "100vh",
         maxWidth: "375px",
         margin: "0 auto",
         padding: "0 20px",
-      }}
-    >
-      <div>This is Welcome Page</div>
-    </Layout>
+      }} >
+
+      <div className="container-fluid">
+        <div className="App">
+          <img
+            className="img-fluid"
+            src="/project-image.png"
+            alt="wait a moment please.."
+          />
+        </div>
+        <div className="description">
+          <h1>
+            <b>Your Job Search, Made Smarter</b>
+          </h1>
+          <p>
+            Every job search is a journey of growth and opportunity. Stay
+            positive and keep moving forward
+          </p>
+
+          <button className="button1">
+            {" "}
+            <b>Get Started </b>
+          </button>
+          <button className="button2">
+            <b>Have an account? Login</b>{" "}
+          </button>
+        </div>
+        
+      </div>
+      </Layout>
+    </>
   );
 }
+
+
+
