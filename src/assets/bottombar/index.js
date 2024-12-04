daebe4cbefcb7536a535e60cb74b4a805cb3a04c
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-12-04 16:34:14 +0000

    Add: Bottom Bar component and its icons

---
+++ src/assets/bottombar/index.js
@@ -0,0 +1,26 @@
+// svg's
+import HomeIconOutlined from "./home-outlined.svg";
+import HomeIconFilled from "./home-filled.svg";
+import BriefcaseIconOutlined from "./briefcase-outlined.svg";
+import BriefcaseIconFilled from "./briefcase-filled.svg"
+import UserIconOutlined from "./user-outlined.svg";
+import UserIconOutlinedBlue from "./user-outlined-blue.svg";
+import UserIconFilled from "./user-filled.svg"
+import ApplicationsIconOutlinedBlue from "./applications-outlined-blue.svg";
+import ApplicationsIconOutlined from "./applications-outlined.svg";
+import ApplicationsIconFilled from "./applications-filled.svg";
+import LogOutIcon from "./logout-icon.svg";
+
+export {
+    HomeIconOutlined,
+    HomeIconFilled,
+    BriefcaseIconOutlined,
+    BriefcaseIconFilled,
+    UserIconOutlined,
+    UserIconFilled,
+    UserIconOutlinedBlue,
+    ApplicationsIconFilled,
+    ApplicationsIconOutlined,
+    ApplicationsIconOutlinedBlue,
+    LogOutIcon
+};
---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+  BriefcaseIconOutlined,
+  BriefcaseIconFilled,
+  UserIconOutlined,
+  UserIconFilled,
+  HomeIconFilled,
+  HomeIconOutlined,
+  ApplicationsIconFilled,
+  ApplicationsIconOutlined
+} from "../assets/bottombar";
+import { NavLink, useLocation } from 'react-router-dom';
+
+export default function BottomBar() {
+
+  const { pathname } = useLocation();
+
+  return (
+    <div
+      className="bg-white position-fixed bottom-0 text-center w-100 shadow-lg d-flex align-items-center justify-content-around"
+      style={{ height: "70px" }}
+    >
+      <NavLink to="/" className="border-0 bg-transparent">
+        <img
+          src={pathname === "/" ? HomeIconFilled : HomeIconOutlined}
+          alt="home icon"
+        />
+      </NavLink>
+      <NavLink to="/my-applications" className="border-0 bg-transparent">
+        <img
+          src={
+            pathname === "/my-applications"
+              ? ApplicationsIconFilled
+              : ApplicationsIconOutlined
+          }
+          alt="briefcase icon"
+        />
+      </NavLink>
+      <NavLink to="/components" className="border-0 bg-transparent">
+        <img
+          src={
+            pathname === "/components"
+              ? BriefcaseIconFilled
+              : BriefcaseIconOutlined
+          }
+          alt="briefcase icon"
+        />
+      </NavLink>
+      <NavLink to="/profile" className="border-0 bg-transparent">
+        <img
+          src={pathname === "/profile" ? UserIconFilled : UserIconOutlined}
+          alt="user icon"
+        />
+      </NavLink>
+    </div>
+  );
+}
