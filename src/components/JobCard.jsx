9fd0087b4331b7b9a09ac3ce19f39812118a3386
OisinSD <oisin.s.dillon@gmail.com>
2024-12-05 23:56:44 +0000

    update to JobCard and styles.css to match design of job card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Button } from "antd";
 import { EnvironmentOutlined, HeartOutlined } from "@ant-design/icons";
-import "../styles.css"; // Import the CSS file
+import "../styles.css"; 
 
 const JobCard = ({
   onClick,
@@ -41,76 +41,3 @@ const JobCard = ({
 };
 
 export default JobCard;
-
-
-
-// import React from "react";
-// import { Card, Tag, Typography } from "antd";
-// import { EnvironmentOutlined, CalendarOutlined } from "@ant-design/icons"; //icon for location and calendar
-// const { Title, Text } = Typography; //taking title and text components from antd
-
-// //helper functions
-
-// /***  THis removes the HTML Tags from the API job Descriptions ***/
-// // const RemoveHTML = (html) => {
-// //   const doc = new DOMParser().parseFromString(html, "text/html");
-// //   return doc.body.textContent || "";
-// // };
-// // /*** max string size is determined in return function (set to 150)
-// // //This makes it so the entire job decription doesnt get printed on job card***/
-// // const shortenText = (html, maxlength) => {
-// //   const cleanText = RemoveHTML(html);
-// //   return cleanText.length > maxlength
-// //     ? cleanText.slice(0, maxlength) + "..."
-// //     : cleanText;
-// // };
-// // /*** this will remove the time from the date to just give YY/MM/DD ***/
-// // const formatDate = (dateString) => {
-// //   const date = new Date(dateString);
-// //   return date.toLocaleDateString("en-GB");
-// // };
-
-// const JobCard = ({
-//   onClick,
-//   title,
-//   company,
-//   location,
-//   salary,
-//   /***  jobType, in the API jobtype just shows "job" so i did not inlude in job card ***/
-//   // description,
-//   // publicationDate,
-//   // levels,
-// }) => {
-//   return (
-//     <div className="job-card-container">
-//       <Card className="job-card" hoverable onClick={onClick}>
-//         <div className="job-card-header">
-//       <img src="https://via.placeholder.com/40?text=Logo" alt={"{company} Logo"} className="company-logo" />
-//         <div>
-//           <div className="job-card-content">
-//           <Text className="company-name">{company}</Text>
-//           <Title level={4}>{title}</Title>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="location">
-//           <EnvironmentOutlined style={{ marginRight: 8 }} />
-//           <Text>{location}</Text>
-//         </div>
-
-//         {/* <div className="mt-2 date">
-//           <CalendarOutlined style={{ marginRight: 8 }} />
-//           <Text>{formatDate(publicationDate)}</Text>
-//         </div> */}
-
-//         {/* <div className="tag-container">
-//           <Tag color="blue">{levels}</Tag>
-//         </div> */}
-//         {/* <p className="mt-2">{shortenText(description, 150)}</p> */}
-//       </Card>
-//     </div>
-//   );
-// };
-
-// export default JobCard;
