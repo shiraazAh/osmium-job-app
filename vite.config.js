e42935b6628d26e0860337e3e882e7ae9b1998ed
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-12-18 20:54:46 +0000

    Update: vite config and website logo

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 /* Contributers: Oliver,  */
 /* Finally managed to connected the backend database using the help of the LLMs ChapGPT & Claude.Ai */
 export default defineConfig({
-  server: {
-    proxy: {
-      "/application": {
-        target: "https://jcxe983h1e.execute-api.eu-west-1.amazonaws.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/application/, "/application"),
-      },
-    },
-  },
   plugins: [react()],
 });
